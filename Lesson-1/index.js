82847fd30e7280e3642161d8d8aa565a9c39bbb2
FozilAzimov <foziljon.azimov@yandex.ru>
2023-01-25 01:19:00 +0500

    1-dars. JS veriable, data types

--- Lesson-1/index.js
+++ Lesson-1/index.js
@@ -1,3 +1,4 @@
-//Pull
-// Pull => bu 1-fileni codelarini o'zgartirib 2-filega copy qilib olish.
-// 1. git pull => master branchidagi index.js fileni branchName branchidagi index.js filega copy qilish.
+//  JavaScript: veriable, data types;
+
+// 1. veriable: var, let, const;
+// 2. data types: String, Number, Undifined, Null, Blean, Symbol, Object;
