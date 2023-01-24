843771940da72381ee299dc401c1d2294372ba88
FozilAzimov <foziljon.azimov@yandex.ru>
2023-01-25 01:04:13 +0500

    Github haqida

--- Github_darsi/review.js
+++ Github_darsi/review.js
@@ -32,4 +32,4 @@
 
 //Pull
 // Pull => bu 1-fileni codelarini o'zgartirib 2-filega copy qilib olish.
-// 1. git pull => master branchidagi index.js fileni branchName branchidagi index.js filega copy qilish..
+// 1. git pull => master branchidagi index.js fileni branchName branchidagi index.js filega copy qilish.
