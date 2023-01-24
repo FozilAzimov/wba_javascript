13da54bc92fe40974feeb618b75e263021523fe3
FozilAzimov <foziljonazimov@yandex.ru>
2023-01-24 17:18:03 +0500

    Github haqida

---
+++ review.js
@@ -0,0 +1,11 @@
+//Branch
+// Branch => bu githubda filial vazifasini bajaradi.Agar yangi branch ochmoqchi bo'lsangiz
+// VS code terminaliga:
+// 2. git branch => qanday branch borligini chiqarib beradi.
+// 3. git checkout -b branchName => yangi branchName degan branch ochib beradi.
+// 4. git switch master => branchName branchdan master branchiga o'tish.
+
+
+//Pull
+// Pull => bu 1-fileni codelarini o'zgartirib 2-filega copy qilib olish.
+// 1. git pull => master branchidagi index.js fileni branchName branchidagi index.js filega copy qilish.
