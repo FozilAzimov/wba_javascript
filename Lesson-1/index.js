5f64c1f23f40b2806a4d67c597766b1d5b835eb0
FozilAzimov <foziljon.azimov@yandex.ru>
2023-01-25 07:58:15 +0500

    1-dars. JS veriable, data types

--- Lesson-1/index.js
+++ Lesson-1/index.js
@@ -2,3 +2,20 @@
 
 // 1. veriable: var, let, const;
 // 2. data types: String, Number, Undifined, Null, Blean, Symbol, Object;
+
+// var => bunda bitta qiymat belgilab pastdan yana shu name bilan qiymat belgilansa ikkinchi qiymatni o'zlashtirib olavaradi.
+// Misol: 
+// var a = 10;
+// var a = 20;
+// console.log(a);  => manashunda a ning qiymti avtomatik ravishda 20 bo'ladi.
+
+// let => bunda var singari bitta nameli veriablega qiymat berib bo'lmaydi.
+// Misol:
+// let b = 10;
+// let b = 20; => ERROR
+// console.log(b);
+
+// const => bu o'zining nomi bilan constanta hisoblanadi. Bitta qiymat biriktirilgandan so'ng uni o'zgartirib bo'lmaydi.
+// const c = 10;
+// const c = 20; Error
+// console.log(c);
