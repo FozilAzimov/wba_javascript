94b7e37d2238ef896d14a02430572490b81e8df1
FozilAzimov <foziljon.azimov@yandex.ru>
2023-01-26 23:21:49 +0500

    '2-dars.

--- Lesson2/Vazifalar Tayyori/index.js
+++ Lesson2/Vazifalar Tayyori/index.js
@@ -1,2 +1,13 @@
-// Assalomu alaykum!
-// Va alaykum assalom!
+// type convertion => bu qiymat data typening o'zgarishi tushiniladi;
+//1. Stringdan Numberga  o'tish:
+// let num = '10';
+// console.log(typeof num);
+// console.log(typeof +num);
+// console.log(typeof Number(num));
+// console.log(typeof parseInt(num));
+// console.log(typeof parseFloat(num));
+
+
+// let text = 10;
+
+// console.log(typeof text.toString(10));
