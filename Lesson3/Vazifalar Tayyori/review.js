4b2816e8c82748fb0c777b59e8c226cf69967bd3
FozilAzimov <foziljon.azimov@yandex.ru>
2023-01-31 20:51:56 +0500

    if else, ternary operator, nullish

--- Lesson3/Vazifalar Tayyori/review.js
+++ Lesson3/Vazifalar Tayyori/review.js
@@ -15,6 +15,9 @@
 // else{
 //   console.log("Noto'g'ri qiymat kiritdingiz!");
 // }
+//  Xulosa: condition har doim yoki true, yoki false qiymat qaytaradi. false qiymat qaytarsa condition blokining ichidagi codelar ishlamaydi.
+// Keyingi conditionlarga tekshirish uchun o'tadi. Qay birida true qaytsa shuni bloki ishlaydi. Agar hamma conditionlar false qaytarsa oxiri else blokining ichi ishlaydi.
+
 
 // 2) ternary operator;
 // let text = "Assalom";
@@ -25,10 +28,12 @@
 // : text === "Buzz"
 // ? console.log("Natija Buzz")
 // : console.log("Noto'g'ri qiymat kiritdingiz!");
+// Xulosa: ternary operator ishlatilishi faqat ikkitalik tekshiruv holatlarida ishlatilsa maqsadga muvofiq bo'ladi.
+// Ko'p tekshiruv holatlarida ham ishlatsa bo'ladi, lekin codelar chalkashib ketishi mumkin va bir qarashada nima vazifa qaytarishini anglab bo'lmaydi.
 
 // 3) Nullish;
 // Nullish (??) huddi Logical operatordagi AND, OR ga o'xshab ishlaydi,
-// lekin nullishdagi belgi ikkita so'roq bo'ladi va bu tekshirub turi null va undefined qiymatlarda false,
+// lekin nullishdagi belgi ikkita so'roq bo'ladi va bu tekshiruv turi null va undefined qiymatlarda false,
 // qolgan qiymatlarda true qiymat qaytaradi;
 
 // let a = true;
@@ -42,3 +47,16 @@
 
 // let a = false;
 // console.log(a ?? undefined); //=> Natija false
+
+// console.log(undefined ?? null); //=> Natija null. chunki ikkita holarham false qaytargani uchun oxirgi qiymatni natijaga chiqaradi;
+
+// let num = 0;
+// let text = "Azimov"
+// console.log(num || 1); // => 1 chunki num ning qiymati false;
+// console.log(1 || num); // => 1 chunki true ekanligi;
+// console.log(num && 1); // => 0 chunki umumiy natija false bo'lgani uchun false o'rnida keluvchi 0 qiymatni chiqaradi;
+// console.log(1 && num); // => 1 chunki ampersantdan oldinda turgan qiymat true bo'lgani uchun keyingi qitmatni tekshirmaydi;
+// console.log(null ?? 1); // => 1 chunki nullishda null qiymat false bo'lgani uchun true qiymat 1 ni chiqaradi;
+// console.log(undefined ?? 1); => 1 chunki nullishda undefined qiymat false bo'lgani uchun true qiymat 1 ni chiqaradi;
+// console.log(text ?? 1); => Azimov chunki nullishda null va undefineddan boshqa hamma qiymatlar true qaytargani uchun
+// birinchi qiymat chiqadi;
