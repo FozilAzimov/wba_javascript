9647acd24d7efc30c5192e7551263a97104de603
FozilAzimov <foziljon.azimov@yandex.ru>
2023-02-01 23:34:44 +0500

    Homework

---
+++ Lesson4/Vazifalar Tayyori/index.js
@@ -0,0 +1,40 @@
+// Homework
+
+// 1 - misol
+// let browser = "Chrome";
+// switch (browser) {
+//   case "Chrome": console.log(`Sizning browseringiz ${browser}`);
+//     break;
+//   case "Firefox": console.log(`Sizning browseringiz ${browser}`);
+//     break;
+//   case "Safari": console.log(`Sizning browseringiz ${browser}`);
+//     break;
+//   case "Opera": console.log(`Sizning browseringiz ${browser}`);
+//     break;
+//   default: console.log("Siz kiritgan qiymat noto'g'ri");
+// }
+
+// 2-misol
+// let a = 2;
+// let b = 3;
+// if (a == 0) console.log(a);
+// else if(a==1)console.log(a);
+// else if(a==2||b==3) console.log(`${a},${b}`);
+// else console.log("Qiymat hato");
+
+// 3-misol
+// let a = 3;
+// while(a){
+//   console.log(a--); //=> bunda natija: 3,2,1 chiqadi va 0ga kelganda condition false natija qaytaradi va loop o'z ishini tugatadi;
+// }
+
+// 4-rasm
+// let i = 0;
+// while(++i<5) console.log(i); // Natija 1,2,3,4 chiqadi 5 ga o'tkanda condition false qaytaradi va loop o'z ishini tugatadi;
+
+// 5-misol
+// let i = 0;
+// while(i++<5) console.log(i); //Bunda Natija 1,2,3,4,5 chiqadi. chunki step 5 ni chiqarib keyin 1 qo'shadi.
+
+// 6-misol
+// for(let i=0; i<3; i++)console.log(`number ${i}`); // Natija number 0, number 1, number 2 chiqadi. Forda step scope ichidagi codening barchasi ishlab bo'lgandan so'ng qo'shadi;
