e49f8eddda4622f32c9179091bbb111c0611db36
FozilAzimov <foziljon.azimov@yandex.ru>
2023-02-02 15:33:58 +0500

    switch case, Loop(for, while, do while)

--- Lesson4/Vazifalar Tayyori/index.js
+++ Lesson4/Vazifalar Tayyori/index.js
@@ -38,3 +38,28 @@
 
 // 6-misol
 // for(let i=0; i<3; i++)console.log(`number ${i}`); // Natija number 0, number 1, number 2 chiqadi. Forda step scope ichidagi codening barchasi ishlab bo'lgandan so'ng qo'shadi;
+// let a = 10;
+
+// 7-misol
+// let num = 100; //Bu qatorda num ning qiymati 20 ga teng bo'ladi;
+// for (let i = 2; i <= num; i++) {  // bunda esa loop ishlaydi. birinchi i=2 bo'ladi va numdan kichikigi tekshiriladi va keyingi qatorga o'tadi.
+//   x = true; // Bu qatorda x o'zgarunchini qiymatini true deb olamiz.
+//   for (let j = 2; j < i; j++) { // Bu qatorda loop ishlaydi va j ning qiymatini 2 deb oladi va i>j ni tekshiradi. Yuqorida ining qiymati 2 edi. 2 hech qachon 2 dan katta bo'lmaydi shuning uchun loop o'z ishini tugatadi.
+//     if (i % j == 0) {
+//       x = false;
+//       break;
+//     }
+//   }
+//   if (x) console.log(i); // bu qatorda x ning qiymati true bo'lgani uchun consolga 2 qiymatni chiqaradi.
+// }
+// Buu sikl yana boshiga qaytadi va i ning qiymatini 1 ga oshiradi.
+// i=3 bo'ladi. Va shu holatda yuqorida aytganimdek dastur 47-line ga keladi.
+//yana i>j ekanligini tekshiradi va true natija oladi. Undan so'ng sikl scope ichiga kiradi va if shrtini tekshiradi.
+//shart 3%2==0 tekshiradi va false natija oladi.if scope ichiga kirmaydi. 47-line dagi loopga yana qaytadi va j nibg qiymati bittaga oshadi.
+//j=3 ga teng bo'lib qoladi.loop endi i>j ligini tekshiradi va false natija oladi va loop o'z ishini tugatadi.
+//x ning qiymati hali ham true ekanligidan consolega i ning qiymati chiqadi yani 3.
+// dastur yana boshiga qaytadi. i ning qiymati 4 bo'ladi. x qiymati true. j=2 holatida i>j tekshiriladi va true natija oladi.
+//loop scope ichi ishlaydi va if sharti tekshiriladi. i%j==0 shart true natija qaytaradi va if scope ichidagi codelar ishlaydi.
+//Shunda x ning qiymati false bo'ladi. break keyin loop ishini tugatadi.
+// keyingi code if x ning qiymati false bo'lgani uchun ining scope ichidagi code ishlamaydi.
+// Xullas shu holatda sikl aylanaveradi. Bizga 100 gacha bo'lgan prime(tub son) larni chiqarib beradi.
--- Lesson4/Vazifalar Tayyori/review.js
+++ Lesson4/Vazifalar Tayyori/review.js
@@ -1,1 +1,59 @@
-// Assalomu alaykum!
+// switch case, Loop(for, while, do while)
+
+// 1). Switch case
+// switch case - bu huddi if else ga o'xshab bir nechta holatlarni tekshirib to'g'ri qiymatni olishga imkon yaratadi;
+//if else dan farqi shundaki switch case har doim aniq bir qiymat kutadi; Biz if else ga o'xshab oraliq berib aniq natija chiqara olmaymiz.
+// Strukturasi:
+// switch(value){
+//   case "value": console.log("Natija chiqadi!");
+//   default:console.log("Yuqoridagi case ishlamasa oxiri default ishlaydi.");
+// } 
+//Bunda switch dan keyin yoziluvchi oddiy qavs ishidagi value case dan keyin tekshiriluvchi qiymatga qat'iy(===) teng bo'lsa keyin ikki nuqtadan keyin yoziluvchi code lar ishlaydi. Aks xolda ishlamaydi.
+
+// 2). For loop
+// looplar nima maqsadda ishlatiladi: Looplar asosan bir qiymatni takroriy chiqarish uchun ishlatiladi;
+// Strukturasi:
+
+// for(boshlangich nuqtasi; tugash nuqtasi; qadami){
+//   code ...
+// }
+
+// looplarda tugash nuqtasi har doim true qiymat kelsa sikl ishlaydi. Agar tugash nuqtasi false qiymat qaytarsa loop o'z ishini butunlay to'xtatadi.
+// for loopda oddiy qavs ichini soddalashtirish mumkin;
+//for(;;){} bu holatda for scope ichida consolga nima ciqarmoqchi bo'lsangiz shi qiymat davriy chiqaveradi.(dastur buziladi.)
+//for(let i=0;;){
+//  console.log(i);
+//  i++;
+//} Bu holatda natija 0 dan boshlab cheksiz o'suvchi son chiqarib beradi;
+// Shu yerda (var) va (let) veriablelarining falqi yana ko'rinadi;
+// for(var i=0; i<5; i++){
+
+// }
+// console.log(i);// Bunda natija: 5 chiqadi. chunki var global veriable. Buni o'zidan pastdagi hamma joyda ishlatsak bo'ladi
+
+// for(let j=0; j<5; j++){
+
+// }
+// console.log(j); //Bunda natija: j is not defined chiqadi. chunki let scope veriable. Faqat o'zining scope ichida ishlatsa bo'ladi.
+
+// 3). while loop
+// while loopi ning boshlangich nuqtasi whiledan oldin elon qilib qo'yiladi.Keyin while kalit so'zidan keyin oddiy qavsda condition tekshiriladi.
+// While conditioni har doim true yoki false natija kutadi. True bolsa loop scope ichidagi codelar ishlaydi. False bo'lsa ishlamaydi.
+// scope ichida codelar oxiriga loop qadami yozib ketiladi;
+// Strukturasi:
+// let a = 0;
+// while(condition){
+//   code ...
+//   a++;
+// }
+
+// 4). do while loop
+// do while loopi - for va while looplaridan bitta yaqqol farqi condition false natija qaytarsa ham dastur bir marta ishlab keyin o'z ishini tugatadi.
+// Chunki do while da dastur birinchi scope ichidagi codelarni ishga tushirib bo'lganidan so'ng keyin condition tekshiriladi;
+// Strukturasi:
+// let b = 0;
+// do{
+//   code ...
+//   b++;
+// }while(condition); 
+// qolgan holatlari while loopi bilan o'xshashdir.
