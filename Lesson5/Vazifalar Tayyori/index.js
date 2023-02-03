0eb990153280977023f9f4a9b0bfed49a05a52e1
FozilAzimov <foziljon.azimov@yandex.ru>
2023-02-03 22:51:14 +0500

    Homework

---
+++ Lesson5/Vazifalar Tayyori/index.js
@@ -0,0 +1,49 @@
+// Homework
+
+// 1).
+// for(let i=0; i<3; i++){
+//   console.log(`number ${i}!`);
+// }
+
+// let i=0;
+// while(i<3){
+//   console.log(`number ${i}!`);
+//   i++;
+// }
+
+// 2).
+// function checkAge(age){
+//   if(age>18){
+//     return "Ro'yxatdan o'tishingiz mumkin"
+//   }
+//   else{
+//     return "Siz ro'yxatdan o'ta olmaysiz?"
+//   }
+// }
+// console.log(checkAge(22));
+
+// 3).
+// function minNum(a,b){
+//   if(a<b) return a;
+//   else return b;
+// }
+// console.log(minNum(-8,5));
+
+// 4).
+// function powNum(a,b){
+//   return a**b;
+// }
+// console.log(powNum(3,2));
+// console.log(powNum(3,3));
+// console.log(powNum(1,100));
+
+// 5).
+// let ask = (a, b, c) => {
+//   if (a) b();
+//   else c();
+// }
+// ask(
+//   "Do you agree?",
+//   ()=> console.log("You agreeed"),
+//   ()=> console.log("You canceled the execution.")
+// );
