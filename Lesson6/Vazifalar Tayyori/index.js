2c0a4f0a7d6091937e732107187c75528e4058ba
FozilAzimov <foziljon.azimov@yandex.ru>
2023-02-08 10:13:55 +0500

    object, Object.assign(),spread operation,for in,structuredClone(),garbage collection

--- Lesson6/Vazifalar Tayyori/index.js
+++ Lesson6/Vazifalar Tayyori/index.js
@@ -87,3 +87,102 @@
 //   }
 // }
 // searchValue(obj, "qizil"); //=> atirgul, qizil;
+
+// 9).
+// let person = {
+//   id: 1,
+//   name: 'Odil',
+//   age: 78,
+//   child: {
+//     id: 1,
+//     name: 'Ali',
+//     age: 48,
+//     child: {
+//       id: 1,
+//       name: 'Umar',
+//       age: 20
+//     }
+//   }
+// }
+// let {age:newAge1, child:{age}, child:{child:{age:newAge2}}} = person;
+// console.log(newAge1+age+newAge2);
+
+// 10).
+// let obj = {
+//   id: 1, name: 'Usmon', job: 'developer'
+// }
+// let obj2 = structuredClone(obj);
+// obj2.name = "Foziljon";
+// console.log(obj);
+// console.log(obj2);
+
+// 11).
+// let obj = {
+//   name: "Foziljon",
+//   age: 22
+// }
+// let func = (a) => obj.surname = a;
+// func("azimov");
+// console.log(obj);
+
+// 12).
+// let obj = {
+//   id: 1,
+//   name: 'Usmon',
+//   job: 'developer',
+//   practice: 1
+// }
+// if(obj.practice>1)console.log("Qabul qilindi");
+// else console.log("Ming bor uzur");
+
+// 13).
+// let obj = {
+//   id: 1,
+//   name: 'Usmon',
+//   job: 'developer',
+//   practice: 1,
+//   talaba: true
+// }
+// "talaba" in obj ? obj.kiridit = true:obj.kiridit="olinmadi";
+// console.log(obj);
+
+// 14).
+// let obj = {
+//   id: 1,
+//   name: 'Usmon',
+//   age: 34,
+//   year: 1990
+// }
+// if ((2023 - obj.year) === obj.age) console.log("Ma'lumot to'g'ri");
+// else console.log("Ma'lumot hato");
+
+// 15).
+// let obj={
+//   id:1,
+//   title:'the best',
+//   address:{street:"Farobiy",number:'34'}
+// }
+// for(key in obj){
+//   if(typeof obj[key] === "object") console.log(true);
+// }
+
+// 16).
+// let person = {
+//   id: 1,
+//   name: 'Umar',
+//   surname: 'Ismoilov',
+// }
+// person.ielts = 5.5;
+// console.log(person);
+// person.ielts >= 5.5 ? console.log("Qabul qilindi") : console.log("Qayta topshirish kerak");
+// person.student = true;
+// console.log(person);
+// if (person.student) {
+//   person.parol = person.id + parseInt(Math.random() * 10);
+// }
+// console.log(person);
+// let personFunc=(parol,personName)=>{
+//   if(person.name===personName&&person.parol===parol)console.log("Hush kelibsiz");
+//   else console.log("Parol yoki name xato");
+// }
+// personFunc(5,"Umar");
