9ee7bf1721962faadefc0f35d817294df4c6faa8
FozilAzimov <foziljon.azimov@yandex.ru>
2023-02-21 19:00:23 +0500

    github bilan ishlash

--- Github/review.js
+++ Github/review.js
@@ -33,3 +33,10 @@
 //Pull
 // Pull => bu 1-fileni codelarini o'zgartirib 2-filega copy qilib olish.
 // 1. git pull => master branchidagi index.js fileni branchName branchidagi index.js filega copy qilish..
+
+// Folder delete qilish
+// git remote -v
+// ls -l
+// rm -r "folder name"
+// git commit -m "comment"
+// git push origin branch_name
