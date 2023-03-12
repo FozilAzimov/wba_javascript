954f99a94b2e96aa28f4b8d62666f62e6469e5b3
FozilAzimov <foziljon.azimov@yandex.ru>
2023-03-12 19:02:02 +0500

    class basics (constructor,extends,inheritance,super), class advenced (static, '_'public, '#'private, get,set,Array,Object,instenceof,Object.assigin,prototype)

--- Lesson15/Vazifalar Tayyori/index.js
+++ Lesson15/Vazifalar Tayyori/index.js
@@ -1,5 +1,6 @@
 // Homework 
 
+// 1). Errorni aniqlang
 // class Animal {
 //   constructor(name) {
 //     this.name = name
@@ -16,7 +17,7 @@
 // let rabbit = new Rabbit("Assalomu alaykum")
 // console.log(rabbit.name);
 
-// =================================================================
+// ======================================================
 
 // Bu xolatda xatolik Rabbit extends Animal ga bo'lganida constructor ichida ( super() ) methodni yozib ketish shart.
 
@@ -38,3 +39,30 @@
 
 // let rabbit = new Rabbit("Assalomu alaykum")
 // console.log(rabbit.name, rabbit.created);
+
+
+// 2). Errorni aniqlang
+// class Rabbit extends Object {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let rabbit2 = new Rabbit("Rab");
+
+// console.log(rabbit2.hasOwnProperty('name')); 
+
+// =======================================================
+
+// Bu holatda biz Rabbit class ichidagi construktor ichiga super() keywordini yozishimiz kerak.
+
+// class Rabbit extends Object {
+//   constructor(name) {
+//     super();
+//     this.name = name;
+//   }
+// }
+
+// let rabbit2 = new Rabbit("Rab");
+
+// console.log(rabbit2.hasOwnProperty('name')); 
