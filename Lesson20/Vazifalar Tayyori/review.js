d3f5cdc8684d754f5aacac6a4c6ea3d5306846da
FozilAzimov <foziljon.azimov@yandex.ru>
2023-03-29 13:25:18 +0500

    clientWidth,clientHeight,clientTop,clientLeft,offsetWidth,offsetHeight,offsetTop,offsetLeft,scrollTop,scrollLeft,scrollHeight,scrollWidth,scroll,scrollBy,scrollTo,scrollX,scrollY,onclick,event,this,onmouseup(),onmouseover(),onmouseout(),onmousemove(),onmouseenter(),onmousedown(),addEventListener()

---
+++ Lesson20/Vazifalar Tayyori/review.js
@@ -0,0 +1,42 @@
+// let parent = document.querySelector(".box");
+
+// console.log(parent.clientHeight);
+// console.log(parent.clientWidth);
+
+// console.log(parent.offsetWidth);
+// console.log(parent.offsetHeight);
+// console.log(parent.offsetLeft);
+// console.log(parent.offsetTop);
+
+// console.log(parent.scrollHeight);
+// console.log(parent.scrollTop);
+
+// function func() {
+//   if (parent.offsetHeight === parent.scrollHeight) {
+//     parent.style.height = '200px';
+//   }
+//   else {
+//     parent.style.height = `${ parent.scrollHeight }px`;
+//   }
+// }
+
+// let func = () => {
+//   parent.scroll(0, parent.scrollHeight);
+// }
+// let func = () => {
+//   parent.scrollTo(0, 300);
+// }
+// let func = () => {
+//   parent.scrollBy(0, 100);
+// }
+
+// console.log(window.scrollY);
+// console.log(window.scrollX);
+
+// function func(e) {
+//   console.log('Salom',e.pageX, e.pageY);
+// }
+
+// function func(e) {
+//   console.log(e.inneText);
+// }
