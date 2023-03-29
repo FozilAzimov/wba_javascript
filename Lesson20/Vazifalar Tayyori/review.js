b4d5d2eb02cfeac5a49bf100d0bc1ea3a0a40828
FozilAzimov <foziljon.azimov@yandex.ru>
2023-03-29 13:31:35 +0500

    clientWidth, clientHeight, clientTop, clientLeft, offsetWidth, offsetHeight, offsetTop, offsetLeft, scrollTop, scrollLeft, scrollHeight, scrollWidth, scroll, scrollBy, scrollTo, scrollX, scrollY, onclick, event, this, onmouseup(), onmouseover(), onmouseout(), onmousemove(), onmouseenter(), onmousedown(), addEventListener()

--- Lesson20/Vazifalar Tayyori/review.js
+++ Lesson20/Vazifalar Tayyori/review.js
@@ -40,3 +40,69 @@
 // function func(e) {
 //   console.log(e.inneText);
 // }
+
+// let div = document.querySelector(".parent");
+// console.log("Width", div.clientWidth);
+// console.log("Height", div.clientHeight);
+
+// console.log(div.clientLeft);
+// console.log(div.clientTop);
+
+// console.log(div.offsetWidth);
+// console.log(div.offsetHeight);
+// console.log(div.offsetLeft);
+// console.log(div.offsetTop);
+
+// console.log(div.scrollTop);
+// console.log(div.scrollLeft);
+// console.log(div.scrollHeight);
+// console.log(div.scrollWidth);
+
+// function func() {
+//   div.scroll(0, div.scrollHeight / 2)
+// }
+
+// div.scrollTo(0, div.scrollHeight)
+
+// function func() {
+//   console.log(window.scrollY);
+//   console.log(window.scrollX);
+// }
+
+// function func(i) {
+//   console.log(i.target);
+// }
+// function func1(i) {
+//   console.log(i);
+// }
+
+
+// import { users } from './data.js';
+// let data = users;
+
+// function del(id) {
+//   let res = data.filter(v => v.id !== id);
+//   data = res;
+//   console.log(data);
+//   data.innerHTML = null;
+//   readData();
+// }
+
+// function readData() {
+//   data.forEach(value=> {
+//     let btn = document.createElement("button");
+//     btn.innerHTML = "Delete";
+//     btn.addEventListener("click", () => del(value.id));
+//     let h2 = document.createElement("h2");
+//     h2.innerText = `${ value.name }`;
+//     h2.append(btn);
+//     div.append(h2);
+//   })
+// }
+// readData();
+
+// parent.addEventListener("click", () => func())
+
+// function func() {
+//   parent.style.background = "red";
+// }
