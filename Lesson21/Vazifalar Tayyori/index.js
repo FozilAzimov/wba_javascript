ec08ad692e69f7b49d97306449ad778d39c6a1d5
FozilAzimov <foziljon.azimov@yandex.ru>
2023-03-30 16:43:17 +0500

    Homework

---
+++ Lesson21/Vazifalar Tayyori/index.js
@@ -0,0 +1,61 @@
+
+// 1-mashq boshlanishi
+
+// let parent = document.querySelector(".parent");
+// let koptok = document.querySelector(".koptok");
+
+// let nuqta = "................................................................";
+// for (let i = 0; i <= 7; i++) {
+//   parent.append(`${ nuqta }\n`);
+// }
+
+// function func(e) {
+//   let parentKordinata = e.target.getBoundingClientRect();
+
+//   let top = e.clientY - parentKordinata.top - parent.clientTop - koptok.clientHeight / 2;
+//   let left = e.clientX - parentKordinata.left - parent.clientLeft - koptok.clientWidth / 2;
+
+//   if (top < 0) top = 0;
+
+//   if (left < 0) left = 0;
+
+//   if (left + koptok.clientWidth > parent.clientWidth) {
+//     left = parent.clientWidth - koptok.clientWidth;
+//   }
+
+//   if (top + koptok.clientHeight > parent.clientHeight) {
+//     top = parent.clientHeight - koptok.clientHeight;
+//   }
+
+//   koptok.style.left = left + 'px';
+//   koptok.style.top = top + "px";
+// }
+
+// 1-mashq tugashi
+
+
+// 2-mashq boshlanishi
+// let strelka = document.querySelector("img");
+// let ul = document.querySelector("ul");
+
+// function func() {
+//   ul.classList.toggle("hidden");
+//   strelka.classList.toggle("str");
+// }
+// 2-mashq tugashi
+
+
+
+// 3-mashq boshlanishi
+// let parent = document.querySelectorAll(".child");
+// for (let child of parent) {
+//   child.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+//   child.firstChild.onclick = () => child.remove();
+// }
+// 3-mashq tugashi
+
+
+
+// 4-mashq boshlanishi
+
+// 4-mashq tugashi
---
+++ Lesson21/Vazifalar Tayyori/review.js
@@ -0,0 +1,15 @@
+// function clicked() {
+//   console.log("click");
+// }
+// function mousedown() {
+//   console.log("mousedown");
+// }
+// function mouseover() {
+//   console.log("mouseover");
+// }
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => clicked());
+// btn.addEventListener("mousedown", () => mousedown());
+// btn.addEventListener("mouseover", () => mouseover());
